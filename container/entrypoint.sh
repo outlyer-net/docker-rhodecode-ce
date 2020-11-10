dd93067f6d4bb80c3a5d46e4bfab0fa883b104d5
Toni Corvera <outlyer@gmail.com>
2020-11-10 15:21:17 +0100

    Adjust repos path

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -4,6 +4,6 @@
 sudo chown -R `id -u`.`id -g` \
     ${RHODECODE_INSTALL_DIR}/community-1 \
     ${RHODECODE_INSTALL_DIR}/vcsserver-1 \
-    ~/repos
+    ${RHODECODE_REPO_DIR}
 
 exec supervisord -c ${RHODECODE_INSTALL_DIR}/supervisor/supervisord.ini
